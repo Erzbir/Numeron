50ced01a7f924c49b5a507bc63531c7bd09af641
Hamusuta0320 <2326955513@qq.com>
2023-06-06 15:14:19 +0800

    add shadowJar to package numeron-console to jar file.

--- numeron-console/build.gradle.kts
+++ numeron-console/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
   kotlin("jvm")
+  id("com.github.johnrengelman.shadow") version "7.1.1"
 }
 
 dependencies {
@@ -9,4 +12,10 @@ dependencies {
   implementation(project(":numeron-menu"))
   implementation(project(":numeron-boot"))
   implementation(project(":numeron-deps"))
+}
+
+tasks.withType<ShadowJar>() {
+  manifest {
+    attributes["Main-Class"] = "com.erzbir.numeron.console.NumeronBotApplication"
+  }
 }
