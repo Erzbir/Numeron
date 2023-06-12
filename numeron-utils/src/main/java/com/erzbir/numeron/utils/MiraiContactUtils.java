a25f6ade00172b3488c550046165d8e0c6475fb6
Erzbir <erzbir@mail.com>
2023-06-13 01:18:38 +0800

    [utils: refactor] 重构获取逻辑

--- numeron-utils/src/main/java/com/erzbir/numeron/utils/MiraiContactUtils.java
+++ numeron-utils/src/main/java/com/erzbir/numeron/utils/MiraiContactUtils.java
@@ -6,8 +6,9 @@ import net.mamoe.mirai.contact.ContactList;
 import net.mamoe.mirai.contact.Friend;
 import net.mamoe.mirai.contact.Group;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
  * @author Erzbir
@@ -15,32 +16,52 @@ import java.util.stream.Collectors;
  * <p>用于获取联系人</p>
  */
 public class MiraiContactUtils {
-    private static final ContactList<Friend> friendList;
-    private static final ContactList<Group> groupList;
+    private static final Map<Long, ContactList<Friend>> friendMap = new HashMap<>();
+    private static final Map<Long, ContactList<Group>> groupMap = new HashMap<>();
 
     static {
         List<Bot> botList = Bot.getInstances();
-        groupList = botList.stream().flatMap(bot -> bot.getGroups().stream()).collect(Collectors.toCollection(ContactList::new));
-        friendList = botList.stream().flatMap(bot -> bot.getFriends().stream()).collect(Collectors.toCollection(ContactList::new));
+        botList.forEach(t -> {
+            friendMap.put(t.getId(), t.getFriends());
+            groupMap.put(t.getId(), t.getGroups());
+        });
     }
 
     private MiraiContactUtils() {
 
     }
 
+    public static Group getGroup(long botId, long id) {
+        return (Group) getContact(botId, id, Group.class);
+    }
+
+    public static Friend getFriend(long botId, long id) {
+        return (Friend) getContact(botId, id, Friend.class);
+    }
+
     public static Group getGroup(long id) {
-        return groupList.get(id);
+        return (Group) getContact(id, Group.class);
     }
 
     public static Friend getFriend(long id) {
-        return friendList.get(id);
+        return (Friend) getContact(id, Friend.class);
     }
 
     public static Contact getContact(long id, Class<? extends Contact> type) {
+        for (Bot bot : Bot.getInstances()) {
+            Contact contact = getContact(bot.getId(), id, type);
+            if (contact != null) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    public static Contact getContact(long botId, long id, Class<? extends Contact> type) {
         if (type.equals(Group.class)) {
-            return getGroup(id);
+            return groupMap.get(botId).get(id);
         } else if (type.equals(Friend.class)) {
-            return getFriend(id);
+            return friendMap.get(botId).get(id);
         }
         return null;
     }
