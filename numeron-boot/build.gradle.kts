55a4db8367fd6157353d6b39b3ede86f65cebd4c
Erzbir <erzbir@mail.com>
2023-06-17 04:37:03 +0800

    [build] 使用函数引入依赖

--- numeron-boot/build.gradle.kts
+++ numeron-boot/build.gradle.kts
@@ -6,30 +6,37 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":numeron-core"))
-    implementation(project(":numeron-api"))
-    implementation(project(":numeron-utils"))
-    implementation(project(":numeron-menu"))
-    implementation(project(":numeron-deps"))
-    implementation(project(":numeron-console"))
+    val unUseSubModule: List<String> = listOf(
+        "numeron-plugin",
+        "numeron-boot"
+    )
+    val unUsePluginSubModule: List<String> = listOf()
 
-    implementation(project(":numeron-plugin:card-search"))
-    implementation(project(":numeron-plugin:chat"))
-    implementation(project(":numeron-plugin:check-weight"))
-    implementation(project(":numeron-plugin:code-process"))
-    implementation(project(":numeron-plugin:help"))
-    implementation(project(":numeron-plugin:openai"))
-    implementation(project(":numeron-plugin:plugin-control"))
-    implementation(project(":numeron-plugin:qq-manage"))
-    implementation(project(":numeron-plugin:rss"))
-    implementation(project(":numeron-plugin:sign"))
-    implementation(project(":numeron-plugin:switch"))
+    fun importModule() {
+        rootProject.subprojects.filter { unUseSubModule.contains(it.name).not() }.forEach {
+            implementation(it)
+        }
+    }
+
+    fun importPlugins() {
+        rootProject.subprojects.firstOrNull { it.name == "numeron-plugin" }?.let { project ->
+            project.subprojects.filter { unUsePluginSubModule.contains(it.name).not() }
+                .forEach {
+                    runtimeOnly(it)
+                }
+        }
+    }
+
+    importModule()
+    importPlugins()
 }
+
 
 tasks.withType<ShadowJar>() {
     manifest {
         attributes["Main-Class"] = "com.erzbir.numeron.NumeronBotApplication"
         attributes["Multi-Release"] = "true"
     }
+    mergeServiceFiles()
     archiveBaseName.set("numeron")
 }
